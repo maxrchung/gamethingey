71ba730cbe79a4be55cde547635b36b2f9188781
Winston Liang <winstoyl@uci.edu>
2015-12-27 14:22:50 -0800

    FREE BEER FOREVER

--- DrunkFight/Assets/Scripts/ItemDropScript.cs
+++ DrunkFight/Assets/Scripts/ItemDropScript.cs
@@ -5,19 +5,44 @@ public class ItemDropScript : MonoBehaviour {
 
      public int currentItem;
      private float dropTime;
+     public Sprite spawn;
+     public Sprite item1;
+     private SpriteRenderer spriteKun;
 
 	// Use this for initialization
 	void Start () {
-          currentItem = (int)(UnityEngine.Random.value*100)%4;
+          spriteKun = GetComponent<SpriteRenderer>();
+          currentItem = (int)(UnityEngine.Random.value*100) % 4 + 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-          if (currentItem == 0 && Time.time - dropTime > 20)
+          if (currentItem == 0 && Time.time - dropTime > 3)
           {
-               currentItem = (int)(UnityEngine.Random.value * 100) % 4;
+               currentItem = (int)(UnityEngine.Random.value * 100) % 4 + 1;
           }
 	}
+
+     void LateUpdate()
+     {
+          if (currentItem == 0)
+          {
+               spriteKun.sprite = spawn;
+          }
+          else
+          {
+               spriteKun.sprite = item1;
+          }
+     }
+
+     void OnTriggerEnter2D(Collider2D other)
+     {
+          if (currentItem != 0)
+          {
+               Debug.Log("dolan"); 
+               takeItem();
+          }
+     }
 
      void takeItem()
      {
