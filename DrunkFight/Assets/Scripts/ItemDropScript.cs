f2e9e9c5661f8b1a501bfd692b50a8b47d55e5b9
Winston Liang <winstoyl@uci.edu>
2015-12-27 13:19:43 -0800

    Added folders and Bad Winston Script and wall

---
+++ DrunkFight/Assets/Scripts/ItemDropScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDropScript : MonoBehaviour {
+
+     public int currentItem;
+     private float dropTime;
+
+	// Use this for initialization
+	void Start () {
+          currentItem = (int)(UnityEngine.Random.value*100)%4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          if (currentItem == 0 && Time.time - dropTime > 20)
+          {
+               currentItem = (int)(UnityEngine.Random.value * 100) % 4;
+          }
+	}
+
+     void takeItem()
+     {
+          currentItem = 0;
+          dropTime = Time.time;
+     }
+}
