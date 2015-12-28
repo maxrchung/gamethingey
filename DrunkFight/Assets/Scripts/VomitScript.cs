ae1b06d18e69748cac4748dfd7b37cd9337a3974
Winston Liang <winstoyl@uci.edu>
2015-12-28 12:38:20 -0800

    vomit added

---
+++ DrunkFight/Assets/Scripts/VomitScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VomitScript : MonoBehaviour {
+
+     public float vomitSpeed;
+     void Update()
+     {
+          transform.position += transform.up * Time.deltaTime * vomitSpeed;
+     }
+}
--- DrunkFight/Assets/Scripts/WeaponScript.cs
+++ DrunkFight/Assets/Scripts/WeaponScript.cs
@@ -4,11 +4,52 @@ using System.Collections;
 public class WeaponScript : MonoBehaviour
 {
 
-     private int currentWeapon = 0;
+     public float fistDelay;
+     public float vomitDelay;
+     public float poopDelay;
+     public float flameDelay;
+     public int currentWeapon = 0;
+     public float timer = 0;
+     public GameObject vomit;
 
      public void getWeapon(int weapon)
      {
           currentWeapon = weapon;
      }
+
+     void Update()
+     {
+          if (Input.GetMouseButtonDown(0) && Time.time > timer)
+          {
+               fireVomit(); 
+               /*
+               if (currentWeapon >= 0)
+               {
+
+               }
+               else if (currentWeapon == 1)
+               {
+
+               }
+               else if (currentWeapon == 2)
+               {
+
+               }
+               else if (currentWeapon <= 3)
+               {
+
+               }
+                */
+          }
+     }
+
+     void fireVomit()
+     {
+          timer = Time.time + vomitDelay;
+          Instantiate(vomit, transform.position, transform.rotation);  
+     }
+
+
+
 
 }
