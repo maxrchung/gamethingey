a3f8173df29d16b609dd5fdf4247391a85c3d7cb
Winston Liang <winstoyl@uci.edu>
2015-12-29 22:30:50 -0800

    cmd respawn + map3 canvas change

--- DrunkFight/Assets/Scripts/Movement.cs
+++ DrunkFight/Assets/Scripts/Movement.cs
@@ -229,7 +229,7 @@ public class Movement : NetworkBehaviour
 				}
                 health = startingHealth;
                 //Debug.Log("Respawning");
-                RpcRespawn();
+                CmdRespawn();
 				Debug.Log (numDrinks);
             }
         }
