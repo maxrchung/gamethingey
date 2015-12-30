39e91fd4d069bbd556437e883f9b65cb2866d28d
Nina <ninavolkmuth@gmail.com>
2015-12-29 18:38:11 -0800

    Some new files

---
+++ DrunkFight/Assets/CharSelectPanel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CharSelectPanel : NetworkBehaviour {
+    public LobbyManager lobbyManager;
+    public RectTransform lobbyPanel;
+    public GameObject startButton;
+    public Slider playerSelection;
+    public GameObject hostCover;
+    public List<GameObject> playerPrefabs;
+    public int index;
+    public GameObject prefab;
+
+    public void OnClickReady() {
+    	Debug.Log("PLAYER SELECTED: " + playerSelection.value);
+    	index = (int) playerSelection.value;
+    	prefab = playerPrefabs[(int) playerSelection.value];
+    	Debug.Log(prefab);
+    	if(lobbyManager.isHost) {
+    		lobbyManager.StartHost();
+        	startButton.SetActive(true);
+        	hostCover.SetActive(false);
+    	}
+    	else {
+            lobbyManager.StartClient();
+            startButton.SetActive(false);
+            hostCover.SetActive(true);
+    	}
+    }
+
+    
+}
---
+++ DrunkFight/Assets/LobbyPlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class LobbyPlayerScript : NetworkLobbyPlayer {
+
+	
+	[SyncVar]
+	public GameObject player;
+
+	public GameObject lobbyPanel;
+	public List<GameObject> playerPrefabs;
+
+	void Start () {
+		foreach(GameObject lol in GameObject.FindGameObjectsWithTag("LobbyPanel")) {
+			lobbyPanel = lol;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLocalPlayer) {
+			if(lobbyPanel == null) {
+				foreach(GameObject lol in GameObject.FindGameObjectsWithTag("LobbyPanel")) {
+					lobbyPanel = lol;
+				}
+			}
+			else {
+				player = playerPrefabs[(int) lobbyPanel.GetComponent<LobbyPanel>().slider.value];
+			}
+		}
+	}
+}
